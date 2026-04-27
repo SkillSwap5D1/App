e34b84947e64efd03df5919e8e6433f419e5cba9
Chidiebere-27 <okoyezita512@gmail.com>
2026-04-27 16:27:11 +0100

    feat: create ModerationService with Firestore integration

---
+++ lib/services/moderation_service.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ModerationService {
+  static final ModerationService _instance = ModerationService._internal();
+
+  factory ModerationService() {
+    return _instance;
+  }
+
+  ModerationService._internal();
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+}
