f3e6e4be239ba8a81a95a592d63ce7b418383d48
Alae <ibnoucheikhalae@gmail.com>
2026-05-05 11:22:24 +0100

    Update AuthWrapper to show LoginScreen for unauthenticated users instead of RegisterScreen

--- lib/screens/auth/auth_wrapper.dart
+++ lib/screens/auth/auth_wrapper.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../providers/auth_provider.dart';
 import '../../theme/app_theme.dart';
-import 'register_screen.dart';
+import 'login_screen.dart';
 import '../home/home_shell_screen.dart';
 
 class AuthWrapper extends StatelessWidget {
@@ -30,8 +30,8 @@ class AuthWrapper extends StatelessWidget {
           return const HomeShellScreen();
         }
 
-        // User is not logged in - show RegisterScreen first
-        return const RegisterScreen();
+        // User is not logged in - show LoginScreen first
+        return const LoginScreen();
       },
     );
   }
