df056d3ed83f4d7350f0004a1409723d47a7094e
Alae <ibnoucheikhalae@gmail.com>
2026-05-09 19:32:03 +0100

    Fix: Remove incorrect import of cloud_firestore in user_service_test.dart

--- test/services/user_service_test.dart
+++ test/services/user_service_test.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:skillswap_app/models/user_model.dart';
